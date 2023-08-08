c2aac239d05869be1713e9da0c862250c4d96646
ZhouShen2489 <zs2489@columbia.edu>
2023-08-07 20:23:55 -0400

    Finish the fundamentals part

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,15 @@
+// console.log("Good");
+
+// let x, y;
+// x = y = 25-10-5;
+// console.log(x, y);
+
+const firstName = "Zhou";
+const lastName = "Shen";
+
+const me = `I'm ${firstName}`;
+console.log(me);
+
+const line1 = "I am \n\
+ a good boy";
+console.log(line1);
--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,8 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const abcd = 23;
+
+const calcAge = birthYear => 2047 - birthYear;
+
+console.log(calcAge(1990));
