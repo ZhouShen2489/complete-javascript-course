369fba5c1ec84782bf89b6764dbc847d4cef384f
ZhouShen2489 <zs2489@columbia.edu>
2023-08-16 00:40:06 -0400

    Update script.js

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,10 +1,23 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-const abcd = 23;
+const getKeivin = function () {
+  const temperature = {
+    value: prompt('What is the temperatur in C: '),
+  };
+  debugger;
+  const keivin = Number(temperature.value) + 273;
 
-const calcAge = birthYear => 2047 - birthYear;
+  return keivin;
+};
 
+<<<<<<< HEAD
 console.log(calcAge(1990));
 
 console.log('Bad');
+||||||| c2aac23
+console.log(calcAge(1990));
+=======
+const keivinValue = getKeivin();
+console.log(keivinValue);
+>>>>>>> 61ced4c9a379380677a389885b1bef7659764c2f
