219b953f0008120167d67717be9ac3c4ad747578
ZhouShen2489 <zs2489@columbia.edu>
2023-08-18 11:50:42 -0400

    add greeting function in 10

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+// function as a return
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+// arrow function
+const greet2 = greeting => name => console.log(`${greeting} ${name}`);
+
+const greeter = greet('Hello!');
+console.log(typeof greeter);
+greeter('Zhou');
+
+const greeter2 = greet2('Hi!');
+console.log(typeof greeter2);
+greeter2('Shen');
